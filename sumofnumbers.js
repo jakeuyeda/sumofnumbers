6328cfe47222d6ef3783f7f20fb95ce11ab091a7
jakeuyeda <jakeu@hawaii.edu>
2020-09-21 01:04:27 -1000

    finished exercise

---
+++ sumofnumbers.js
@@ -0,0 +1,38 @@
+const list = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(list));
+
+function sumWhile(nums) {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(list));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(list));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (memo, num) { return memo + num; });
+}
+
+console.log(sumTheSimpleWay(list));
